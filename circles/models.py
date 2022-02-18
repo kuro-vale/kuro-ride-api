8e98f5718c99525ab06e45934873fe9ae815d7e6
kuro-vale <jsalcedo218@gmail.com>
2022-02-18 17:35:39 -0500

    Update Circle models

--- circles/models.py
+++ circles/models.py
@@ -1,6 +1,7 @@
 # Django
 from django.db import models
 # Utils
+from users.models import User, Profile
 from utils.models import KRideModel
 
 
@@ -9,6 +10,7 @@ class Circle(KRideModel):
     name = models.CharField('circle name', max_length=140)
     slug_name = models.SlugField(unique=True, max_length=40)
     about = models.CharField('circle description', max_length=255)
+    members = models.ManyToManyField(User, through='Membership', through_fields=('circle', 'user'))
     picture = models.ImageField(upload_to='users/static/circles/images', blank=True, null=True)
     rides_offered = models.PositiveIntegerField(default=0)
     rides_taken = models.PositiveIntegerField(default=0)
@@ -23,3 +25,20 @@ class Circle(KRideModel):
 
     class Meta(KRideModel.Meta):
         ordering = ['-rides_taken', '-rides_offered']
+
+
+class Membership(KRideModel):
+    """ Membership is the table that hold the relationship between a user and a circle"""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    circle = models.ForeignKey(Circle, on_delete=models.CASCADE)
+    is_admin = models.BooleanField('circle admin', default=False)
+    user_invitation = models.PositiveIntegerField(default=0)
+    remaining_invitation = models.PositiveIntegerField(default=0)
+    invited_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='invited_by')
+    rides_taken = models.PositiveIntegerField(default=0)
+    rides_offered = models.PositiveIntegerField(default=0)
+    is_active = models.BooleanField('status', default=True)
+
+    def __str__(self):
+        return f'@{self.user.username} at #{self.circle.slug_name}'
